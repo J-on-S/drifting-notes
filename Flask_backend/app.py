8d7fc60bab06d7243494e512fecc1be728561f34
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-02-07 17:07:48 -0500

    Put in stubs for the api for now

--- Flask_backend/app.py
+++ Flask_backend/app.py
@@ -2,5 +2,13 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='static')
 
+@app.route('/api/receive')
+def receive_note():
+    return "TODO: receive a note"
+
+@app.route('/api/send')
+def send_note():
+    return "TODO: send a note"
+
 if __name__ == "__main__":
     app.run()
