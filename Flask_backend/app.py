a14a61103c48491ffced6bc331fd684f7d3841c2
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-02-07 18:02:33 -0500

    wrap the str in dumps and see what happens

--- Flask_backend/app.py
+++ Flask_backend/app.py
@@ -1,12 +1,12 @@
-import json
 from flask import Flask, send_from_directory, jsonify
 from mongo_client import insert_note, get_random_note
+from bson.json_util import dumps
 
 app = Flask(__name__, static_folder='static')
 
 @app.route('/api/receive/<anon_id>', methods=['GET'])
 def receive_note(anon_id):
-    return json.loads(str(get_random_note(anon_id)))
+    return dumps(get_random_note(anon_id))
 
 @app.route('/api/send', methods=['POST'])
 def send_note():
