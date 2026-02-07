2e79d5d57cd7a2b38b915f7ed818c80400576a16
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-02-07 17:25:09 -0500

    Add a test endpoint

--- Flask_backend/app.py
+++ Flask_backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+from mongo_client import insert_note, get_random_note
 
 app = Flask(__name__, static_folder='static')
 
@@ -9,6 +10,10 @@ def receive_note():
 @app.route('/api/send')
 def send_note():
     return "TODO: send a note"
+
+@app.route('/test')
+def test_note():
+    return str(get_random_note("USER"))
 
 if __name__ == "__main__":
     app.run()
