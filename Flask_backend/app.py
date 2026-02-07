fe610154ee34499e826ee15036969ffe5dd0a76b
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-02-07 14:59:29 -0500

    Add flask and vercel

---
+++ Flask_backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/<path:path>")
+def send_webpage(path):
+    return send_from_directory("../Hack2026_website", path)
+
+if __name__ == "__main__":
+    app.run()
