61bedebfbe2278d10b446b9dfb0b388d173fb3de
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-02-07 16:10:43 -0500

    Remove simple serve in app.py

--- Flask_backend/app.py
+++ Flask_backend/app.py
@@ -2,9 +2,5 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route("/")
-def simple_index():
-    return "<p>Welcome !</p>"
-
 if __name__ == "__main__":
     app.run()
