1a73de68e43a4d5f2e1127983848d5eb3062b902
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-02-07 16:16:48 -0500

    Use real website as static folder

--- Flask_backend/app.py
+++ Flask_backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='../Hack2026_website')
 
 if __name__ == "__main__":
     app.run()
