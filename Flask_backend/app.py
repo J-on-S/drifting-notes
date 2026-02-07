d3a82c4d01a7c87700855a5432d368b1f560aad1
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-02-07 18:07:27 -0500

    just...see what happens

--- Flask_backend/app.py
+++ Flask_backend/app.py
@@ -1,3 +1,4 @@
+from json import loads
 from flask import Flask, send_from_directory, jsonify
 from mongo_client import insert_note, get_random_note
 from bson.json_util import dumps
@@ -6,7 +7,7 @@ app = Flask(__name__, static_folder='static')
 
 @app.route('/api/receive/<anon_id>', methods=['GET'])
 def receive_note(anon_id):
-    return dumps(get_random_note(anon_id))
+    return jsonify(loads(dumps(get_random_note(anon_id))))
 
 @app.route('/api/send', methods=['POST'])
 def send_note():
