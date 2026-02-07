6611c5c93482bf208deda8cf71ab952d85fc9eb5
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-02-07 17:56:04 -0500

    wrap the str in jsonify and see what happens

--- Flask_backend/app.py
+++ Flask_backend/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 from mongo_client import insert_note, get_random_note
 
 app = Flask(__name__, static_folder='static')
 
 @app.route('/api/receive/<anon_id>', methods=['GET'])
 def receive_note(anon_id):
-    return str(get_random_note(anon_id))
+    return jsonify(str(get_random_note(anon_id)))
 
 @app.route('/api/send', methods=['POST'])
 def send_note():
