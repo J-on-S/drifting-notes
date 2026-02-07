50c6bcec8017cf271b767d4bea76eae04eb625d8
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-02-07 15:38:11 -0500

    Add simple mongo client for testing

---
+++ Flask_backend/mongo_client.py
@@ -0,0 +1,13 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+password = os.environ['DB_PASS']
+uri = "mongodb+srv://jonathansandjong_db_user:" + password + "@mcwics-db.i6qruhs.mongodb.net/?appName=McWiCS-DB"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
