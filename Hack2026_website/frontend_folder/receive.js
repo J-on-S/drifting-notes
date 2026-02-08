7e364331c61e16d9c6ba85d663db1787dc7a3be2
yulinge <yulinge@yulingedeMacBook-Air.local>
2026-02-08 09:09:06 -0500

    changed a bit of send.js

--- Hack2026_website/frontend_folder/receive.js
+++ Hack2026_website/frontend_folder/receive.js
@@ -1,1 +1,3 @@
 //assume database is complete!
+const response = fetch('../api/receive/anon-user-placeholder')
+fetch('../api/receive/anon-user/placeholder').then(res => res.json()).then(data => console.log(data))
