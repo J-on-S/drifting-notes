d41c4104cd8c0e657ee0326135609a94ebe0e945
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-02-07 19:57:59 -0500

    Move frontend into static again

--- Flask_backend/app.py
+++ Flask_backend/app.py
@@ -1,9 +1,13 @@
 from json import loads
-from flask import Flask, send_from_directory, jsonify, request
+from flask import Flask, send_from_directory, jsonify, request, redirect
 from mongo_client import insert_note, get_random_note
 from bson.json_util import dumps
 
 app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def redirect_index():
+    return redirect('/static/driftingnotes.html')
 
 @app.route('/api/receive/<anon_id>', methods=['GET'])
 def receive_note(anon_id):
