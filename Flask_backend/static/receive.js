7c6884251f95f522839e0a608be936407560ce8a
yulinge <yulinge@yulingedeMacBook-Air.local>
2026-02-08 09:34:08 -0500

    add url receive

--- Flask_backend/static/receive.js
+++ Flask_backend/static/receive.js
@@ -1,11 +1,13 @@
 const receiveBtn = document.getElementById("receiveBtn")
 const messageBox = document.getElementById("messageBox")
+const songURL = document.getElementById("url")
 
 receiveBtn.addEventListener("click", () => {
   fetch("../api/receive/random")
     .then(response => response.json())
     .then(data => {
       messageBox.innerText = data.message
+      songURL.innerHTML = data.url
     })
     .catch(err => {
       console.error(err)
