7ee9863a07943d3c7abc59aab166a8f9b3fd1881
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-02-07 19:31:05 -0500

    redirect to website, files in static only

--- Flask_backend/app.py
+++ Flask_backend/app.py
@@ -1,9 +1,11 @@
-from json import loads
-from flask import Flask, send_from_directory, jsonify, request
+from flask import Flask, send_from_directory, jsonify, request, url_for, redirect
 from mongo_client import insert_note, get_random_note
-from bson.json_util import dumps
 
 app = Flask(__name__, static_folder='static')
+
+app.rout('/')
+def redirect_index():
+    return redirect('/static/driftingnotes.html')
 
 @app.route('/api/receive/<anon_id>', methods=['GET'])
 def receive_note(anon_id):
